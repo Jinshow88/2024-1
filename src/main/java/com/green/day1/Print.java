27c2794fba9d680aa68693a0ea6f9e39128dca36
Administrator <ljun88@naver.com>
2024-03-15 12:36:45 +0900

    hi

---
+++ src/main/java/com/green/day1/Print.java
@@ -0,0 +1,18 @@
+package com.green.day1;
+
+//print access >> PrintAccess
+public class Print {
+    public static void main(String[] args ){
+        System.out.print("Hello!한글\n");
+        System.out.print("Hell우히히!\n");
+        System.out.print("Hello!\n");
+
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("우리");
+    }
+}
+
